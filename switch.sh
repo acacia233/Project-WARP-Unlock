2eb6ada4f2bc77897bb985a72bf5f81b360ef350
acacia233 <55625991+acacia233@users.noreply.github.com>
2021-10-03 18:38:20 +0800

    Create switch.sh

---
+++ switch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source /etc/profile
+
+# Variation
+Interface=wg
+Hostname=
+Telegram_Token=
+Telegram_ChatID=
+URL_Message="https://api.telegram.org/bot$Telegram_Token/sendMessage"
+UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
+
+function Start {
+    Test_Netflix_Access
+}
+
+function Test_Netflix_Access {
+    local result="$(curl --interface $Interface --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 5 "https://www.netflix.com/title/81215567" 2>&1)"
+    if [[ "$result" == "200" ]]; then
+        PushNotification
+    else
+        ChangeIP
+    fi
+}
+
+function ChangeIP {
+    wg-quick down $Interface  >/dev/null 2>&1
+    sleep 2
+    wg-quick up $Interface  >/dev/null 2>&1
+    Test_Netflix_Access
+}
+
+function PushNotification {
+    local Message="New WARP IP for Netflix%0ANode:$Hostname"
+    curl -s -X POST $URL_Message -d chat_id=$Telegram_ChatID -d text="$Message" >/dev/null
+}
+
+Start
