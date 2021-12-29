f8468189ed82fc124d47b0d4baf0022cf218fb4d
acacia233 <55625991+acacia233@users.noreply.github.com>
2021-12-29 16:45:08 +0800

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -3,7 +3,7 @@
 source /etc/profile
 
 # Variation
-Interface=wg
+Interface=
 Region=
 Hostname=
 Telegram_Token=
@@ -18,52 +18,37 @@ function Start {
     echo -e " [Intro] Test System:Ubuntu 20"
     echo -e " [Intro] OpenSource-Project:https://github.com/acacia233/Project-WARP-Unlock"
     echo -e " [Intro] Telegram Channel:https://t.me/cutenicobest"
-    echo -e " [Intro] Version:2021-11-3-1"
+    echo -e " [Intro] Version:2021-12-29-1"
     Test_Netflix_Access
 }
 
 function Test_Netflix_Access {
-    result1="$(curl --interface $Interface --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 5 "https://www.netflix.com/title/81215567" 2>&1)"
-    if [[ "$result1" == "200" ]]; then
-        result2="$(curl --interface $Interface --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | cut -d '/' -f4 | cut -d '-' -f1 2>&1)"
-        if [[ "$result2" == "$Region" ]]; then
-            ErrorCount=0
-            Judge
-        fi
-    elif [[ "$result1" == "403" ]]; then
-        ChangeIP
-    else
-        let ErrorCount++
-        if [[ $ErrorCount == 5 ]];then
-            curl -s -X POST $URL_Message -d chat_id=$Telegram_ChatID -d text="Node:$Hostname%0ANeed to manually check" >/dev/null
-            break
-        fi
-        Judge
-    fi
-}
-
-function ChangeIP {
-    wg-quick down $Interface >/dev/null 2>&1
-    sleep 1
-    wg-quick up $Interface >/dev/null 2>&1
-    let Count++
-    Test_Netflix_Access
+    until [[ "$result1" == "200" ]] && [[ "$result2" == "$Region" ]]
+        do
+            local result1="$(curl --interface $Interface --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 5 "https://www.netflix.com/title/81215567" 2>&1)"
+            local result2="$(curl --interface $Interface --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | cut -d '/' -f4 | cut -d '-' -f1 2>&1)"
+            wg-quick down $Interface >/dev/null 2>&1
+            sleep 2
+            wg-quick up $Interface >/dev/null 2>&1
+            let Count++
+        done
+    Judge
 }
 
 function Judge {
     if [[ $Count == 0 ]];then
-        sleep 30
-        echo -e " [Info] Still Working,Skipped..."
+	echo -e " [Info] Still Working,Skipped..."
+        sleep 100
         Test_Netflix_Access
     else
         PushNotification
-        Test_Netflix_Access
+	    Test_Netflix_Access
     fi
 }
 
 function PushNotification {
     local Message="New WARP IP for Netflix%0ANode:$Hostname%0AChange Times:$Count"
-    echo -e " [Info] Change Times:$Count"
+    echo -e " [Info] Finished,Change Times:$Count"
     curl -s -X POST $URL_Message -d chat_id=$Telegram_ChatID -d text="$Message" >/dev/null
     Count=0
 }
